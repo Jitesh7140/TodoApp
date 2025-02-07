311068c3985471f1a4f0f84eaffa3cdd22fb0c3e
Jitesh7140 <jiteshsaini2005@gmail.com>
2025-02-08 01:15:56 +0530

    Initial commit

--- src/Component/Addtodo.jsx
+++ src/Component/Addtodo.jsx
@@ -32,7 +32,7 @@ function Addtodo(props) {
         placeholder="Add a Task"
       />
       <button  onClick={additem} className="bg-red-600 m-2 px-4 py-2 font-bold rounded-xl cursor-pointer hover:bg-red-700">
-        Save
+        S ave
       </button>
 
       </div>
